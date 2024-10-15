325a2254ea72d5e8cc862292552fc89c62051e8f
svetozar12 <s.gospodinov02@gmail.com>
2024-10-15 23:06:45 +0300

    added some gql qol changes

--- apps/TeamFlowApi/src/app/app.module.ts
+++ apps/TeamFlowApi/src/app/app.module.ts
@@ -1,13 +1,22 @@
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
-import { SetResolver } from './set.resolver';
+import { SetResolver } from './set/set.resolver';
+import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
 
 @Module({
   imports: [
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       typePaths: ['./**/*.graphql'],
+      playground: false,
+      definitions: {
+        path: 'apps/TeamFlowApi/src/graphql.ts',
+        outputAs: 'class',
+        emitTypenameField: true,
+        // skipResolverArgs: true,
+      },
+      plugins: [ApolloServerPluginLandingPageLocalDefault()],
     }),
   ],
   providers: [SetResolver],
--- apps/TeamFlowApi/src/app/set/set.resolver.ts
+++ apps/TeamFlowApi/src/app/set/set.resolver.ts
@@ -1,45 +1,40 @@
 import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
-
-export interface SetEntity {
-  id: number;
-  name: string;
-  numParts: number;
-  year: string;
-}
+// eslint-disable-next-line @nx/enforce-module-boundaries
+import { Set } from '@team-flow/apps/TeamFlowApi/src/graphql';
 
 @Resolver('Set')
 export class SetResolver {
-  private sets: SetEntity[] = [
+  private sets: Set[] = [
     {
       id: 1,
       name: 'Voltron',
       numParts: 2300,
-      year: '2019'
+      year: 2019,
     },
     {
       id: 2,
       name: 'Ship in a Bottle',
       numParts: 900,
-      year: '2019'
-    }
+      year: 2019,
+    },
   ];
 
   @Query('allSets')
-  getAllSets(): SetEntity[] {
+  getAllSets(): Set[] {
     return this.sets;
   }
 
   @Mutation()
   addSet(
     @Args('name') name: string,
-    @Args('year') year: string,
+    @Args('year') year: number,
     @Args('numParts') numParts: number
   ) {
     const newSet = {
       id: this.sets.length + 1,
       name,
       year,
-      numParts: +numParts
+      numParts: +numParts,
     };
 
     this.sets.push(newSet);
